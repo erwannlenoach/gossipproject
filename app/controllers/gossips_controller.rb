50a5cecd9cddb503d29b711fc0933139c6d7a598
Erwann Le Noac’h <>
2021-02-10 12:00:15 +0100

    edit OK

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -11,22 +11,36 @@ class GossipsController < ApplicationController
     end
 
     def new
-        # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
+
+    @gossip = Gossip.new # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
+
     end
 
     def create
-       @gossip = Gossip.create(params["gossip_text"])
-       @gossip = Gossip.show 
+        @gossip = Gossip.new
+        post_params = params.require(:gossip).permit(:title, :content)
+        gossip = Gossip.create(post_params)
+        redirect_to gossips_path
+    
         # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
         # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
         # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
     end
 
     def edit
+    
+        @gossip = Gossip.find(params[:id])
+        
         # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
     end
 
     def update
+
+     @gossip = Gossip.find(params[:id])
+     post_params = params.require(:gossip).permit(:title, :content)
+     @gossip.update(post_params)
+     redirect_to gossip_path
+
         # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
         # pour info, le contenu de ce formulaire sera accessible dans le hash params
         # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
@@ -36,6 +50,5 @@ class GossipsController < ApplicationController
         # Méthode qui récupère le potin concerné et le détruit en base
         # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
     end
-end
 
-    
+end 
